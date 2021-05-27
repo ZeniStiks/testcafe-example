76b89dca3b5ec06d720c657c62a50fe64a8f785f
Adomas Racas <adomas.racas@sony.com>
2021-05-27 13:48:49 +0300

    Update all

--- test-data/example-test-data.js
+++
@@ -1,8 +0,0 @@
-export const testData = {
-    validUsername: 'tomsmith',
-    validPassword: 'SuperSecretPassword!',
-    invalidUsername: 'orange',
-    invalidPassword: '12345',
-    invalidUsernameError: 'Your username is invalid!',
-    invalidPasswordError: 'Your password is invalid!'
-};
